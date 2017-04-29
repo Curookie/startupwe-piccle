c0fae4634431a67c0b9f02ab4bd9529fbd5850a7
Curookie <curookie@naver.com>
2017-04-29 19:02:34 +0900

    Start 액티비티 수정

--- app/src/main/java/piccle/startupwe/piccle/StartActivity.java
+++ app/src/main/java/piccle/startupwe/piccle/StartActivity.java
@@ -1,13 +1,38 @@
 package piccle.startupwe.piccle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class StartActivity extends AppCompatActivity {
+
+    private ImageView btnGive, btnTake;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        btnGive = (ImageView)findViewById(R.id.btnGive);
+        btnTake = (ImageView)findViewById(R.id.btnTake);
+
+        btnGive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, GiveRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(StartActivity.this, Take.class);
+//                startActivity(intent);
+            }
+        });
+
     }
 }
