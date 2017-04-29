8f53e2c81a6d28ab6fdd7ce913ae46c3af55d2aa
Kimsup10 <kss5662@ajou.ac.kr>
2017-04-30 05:10:19 +0900

    Implemented view flipper auto slides

--- app/src/main/java/piccle/startupwe/piccle/MainActivity.java
+++ app/src/main/java/piccle/startupwe/piccle/MainActivity.java
@@ -14,6 +14,11 @@ import android.widget.TextView;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.ViewFlipper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,8 +35,23 @@ public class MainActivity extends AppCompatActivity {
         //Gallery gallery = (Gallery)findViewById(R.id.gallery);
         //gallery.setAdapter(new ImageAdapter(this));
         ViewFlipper viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator()); //add this
+        fadeIn.setDuration(1500); //time in milliseconds
+
+
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator()); //and this
+        fadeOut.setStartOffset(1000);
+        fadeOut.setDuration(1500); //time in milliseconds
+
+
+        viewFlipper.setInAnimation(fadeIn);
+        viewFlipper.setOutAnimation(fadeOut);
+
+
         viewFlipper.setAutoStart(true);
-        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setFlipInterval(4000);
         viewFlipper.startFlipping();
 
         loginButton = (Button) findViewById(R.id.loginButton);
