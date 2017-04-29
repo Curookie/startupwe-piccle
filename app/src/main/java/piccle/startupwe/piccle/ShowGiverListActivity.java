5660751a6ae227bcaab996baf4b3ec395d113163
meansoup <alsrnr1210@gmail.com>
2017-04-30 03:48:00 +0900

    button & func

--- app/src/main/java/piccle/startupwe/piccle/ShowGiverListActivity.java
+++ app/src/main/java/piccle/startupwe/piccle/ShowGiverListActivity.java
@@ -2,27 +2,49 @@ package piccle.startupwe.piccle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 public class ShowGiverListActivity extends AppCompatActivity {
 
-    private ListView mListView = null;
-    private ListViewAdapter mAdapter = null;
+//    private ListView mListView = null;
+//    private ListViewAdapter mAdapter = null;
+    Button bDslr;
+    Button bPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_giver_list);
 
-        mListView = (ListView) findViewById(R.id.showGiverListView);
-        mAdapter = new ListViewAdapter(this);
-        mListView.setAdapter(mAdapter);
+//        mListView = (ListView) findViewById(R.id.showGiverListView);
+//        mAdapter = new ListViewAdapter(this);
+//        mListView.setAdapter(mAdapter);
 
-        mAdapter.addItem(getResources().getDrawable(R.drawable.noprofile), "홍", "길동");
-        mAdapter.addItem(getResources().getDrawable(R.drawable.noprofile), "김", "길동");
-        mAdapter.addItem(getResources().getDrawable(R.drawable.noprofile), "이", "길동");
-        mAdapter.addItem(getResources().getDrawable(R.drawable.noprofile), "박", "길동");
-        mAdapter.addItem(getResources().getDrawable(R.drawable.noprofile), "황", "길동");
+//        mAdapter.addItem(getResources().getDrawable(R.drawable.noprofile), "홍", "길동");
+//        mAdapter.addItem(getResources().getDrawable(R.drawable.noprofile), "김", "길동");
+
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.activity_show_giver_list);
+        linearLayout.setBackgroundResource(R.mipmap.img_listdslr);
+
+        bDslr = (Button) findViewById(R.id.dslrB);
+        bPhone = (Button) findViewById(R.id.phoneB);
+
+        bDslr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                linearLayout.setBackgroundResource(R.mipmap.img_listdslr);
+            }
+        });
+        bPhone.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                linearLayout.setBackgroundResource(R.mipmap.img_listphone);
+            }
+        });
+
 
     }
 }
