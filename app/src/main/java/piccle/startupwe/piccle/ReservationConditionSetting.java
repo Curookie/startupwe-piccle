5098dc1c29a25ffaeae9ba622ea49769dc25d1c4
Kimsup10 <kss5662@ajou.ac.kr>
2017-04-29 21:18:57 +0900

    Implemented making and confirming reservations

---
+++ app/src/main/java/piccle/startupwe/piccle/ReservationConditionSetting.java
@@ -0,0 +1,13 @@
+package piccle.startupwe.piccle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConditionSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_condition_setting);
+    }
+}
