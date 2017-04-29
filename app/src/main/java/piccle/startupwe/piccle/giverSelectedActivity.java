52484a662c355ec310c7ad70a98795f939c05b25
meansoup <alsrnr1210@gmail.com>
2017-04-30 07:17:47 +0900

    add activity & make view

---
+++ app/src/main/java/piccle/startupwe/piccle/giverSelectedActivity.java
@@ -0,0 +1,28 @@
+package piccle.startupwe.piccle;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class giverSelectedActivity extends AppCompatActivity {
+
+    private ImageView BresReq;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giver_selected);
+
+        BresReq = (ImageView) findViewById(R.id.requestReservation);
+        BresReq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(giverSelectedActivity.this, MakingReservationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
